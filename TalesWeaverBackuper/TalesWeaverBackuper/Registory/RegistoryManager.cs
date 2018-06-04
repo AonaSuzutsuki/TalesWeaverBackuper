8ac46197184514e31a44c7f7275f166044717987
涼月蒼菜 <rvvfake.org@gmail.com>
2018-06-04 22:19:47 +0900

    Finished LauncherMover

---
+++ TalesWeaverBackuper/TalesWeaverBackuper/Registory/RegistoryManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalesWeaverBackuper.Registory
+{
+    public class RegistoryManager
+    {
+        public static string GetRegistory(string keyPath, string key, RegistryHive hive, RegistryView view)
+        {
+            string value = null;
+
+            try
+            {
+                using (var prerKey = RegistryKey.OpenBaseKey(hive, view))
+                {
+                    using (var rKey = prerKey.OpenSubKey(keyPath))
+                    {
+                        value = (string)rKey.GetValue(key);
+                    }
+                }
+            }
+            catch (Exception) { }
+
+            return value;
+        }
+
+        public static void SetRegistory(string keyPath, string key, RegistryHive hive, RegistryView view, string data)
+        {
+            using (var prerKey = RegistryKey.OpenBaseKey(hive, view))
+            {
+                using (var rKey = prerKey.CreateSubKey(keyPath, RegistryKeyPermissionCheck.ReadWriteSubTree))
+                {
+                    rKey.SetValue(key, data);
+                }
+            }
+        }
+    }
+}
---
+++ TalesWeaverBackuper/TalesWeaverBackuper/Views/LauncherMover.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TalesWeaverBackuper.Views
+{
+    /// <summary>
+    /// LauncherMover.xaml の相互作用ロジック
+    /// </summary>
+    public partial class LauncherMover : Window
+    {
+        public LauncherMover()
+        {
+            InitializeComponent();
+
+            var model = new Models.LauncherMoverModel();
+            var vm = new ViewModels.LauncherMoverViewModel(this, model);
+            DataContext = vm;
+        }
+    }
+}
