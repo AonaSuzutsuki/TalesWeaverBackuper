7db4af51470c895ebe9b2e6695b2a0d1d8873caf
涼月蒼菜 <rvvfake.org@gmail.com>
2018-06-05 01:22:23 +0900

    Added Menu.

--- TalesWeaverBackuper/TalesWeaverBackuper/ViewModels/MainWindowViewModel.cs
+++ TalesWeaverBackuper/TalesWeaverBackuper/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using CommonStyleLib.Models;
 using CommonStyleLib.ViewModels;
+using Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using TalesWeaverBackuper.Models;
 
 namespace TalesWeaverBackuper.ViewModels
@@ -14,6 +16,34 @@ namespace TalesWeaverBackuper.ViewModels
     {
         public MainWindowViewModel(Window view, MainWindowModel model) : base(view, model)
         {
+            this.model = model;
+
+
+
+            LauncherRegisterBtClicked = new DelegateCommand(LauncherRegisterBt_Clicked);
+
+            DoLoaded();
         }
+
+        #region Fields
+        private MainWindowModel model;
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Event Properties
+        public ICommand LauncherRegisterBtClicked { get; set; }
+        #endregion
+
+        #region Event Methods
+
+
+        public void LauncherRegisterBt_Clicked()
+        {
+            model.ShowLauncherRegister();
+        }
+        #endregion
     }
 }
